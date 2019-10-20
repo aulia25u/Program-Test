dda8a2bc1d748807dbf63a8bb0e766775d6a1e79
Aulia Rahman <56797963+auliatenshi@users.noreply.github.com>
2019-10-20 23:33:42 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************
+*	Materi : Materi 4 PBO Operator						           *
+*	Nama   : Aulia Rahman								           *
+*	Kelas  : TK-42-03									           *
+*   Tujuan : Contoh Program Materi 4 Operator				       *
+********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	double d;
+	char c;
+	char *s;
+
+	// mengisi nilai kedalam variabel
+
+	i = 75;
+	d = 8.615;
+	c = 'c';
+	s = (char *) "contoh string";
+
+	//menampilkan nilai variabel
+
+	cout << "nilai i : " << i << endl;
+	cout << "nilai d : " << d << endl;
+	cout << "nilai c : " << c << endl;
+	cout << "nilai s : " << s << endl;
+
+}
